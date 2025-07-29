7c4f5a050a7136536e23d732b152a667c553909a
Aliya Voronkina <atleugabilova@mail.ru>
2025-07-29 10:30:46 +0500

    task

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,5 @@
+public class FormDate {
+    public int day;    // День
+    public int month;  // Месяц
+    public int year;   // Год
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;         // Имя
+    public String surname;      // Фамилия
+    public String patronymic;   // Отчество
+    public String phone;        // Телефон
+    public String passport;     // Паспорт
+    public boolean subscription; // Подписка на SMS
+    public FormDate birthday;   // Вложенный объект даты
+}
